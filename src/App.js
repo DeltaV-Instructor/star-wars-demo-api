55a9aa4de3c53c28556ce006338dc6c6d2f9114b
tektechnologies <info@tektechnologies.com>
2022-07-30 23:40:26 -0500

    add env var

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React from 'react';
-// 5. talk about npm and axios and add it to our current package
+// 3. talk about npm and axios and add it to our current package
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      value: ''
+      value: '',
+      starWarsChars: []
     };
   }
 
 // 2. create ()
-handleSubmit = (event) => {
+// handleSubmit = (event) => {
+  handleSubmit = async (event) => {
   event.preventDefault();
   console.log('Submit Event: ',event);
-  //3. Make request to star wars api
+  //4. Make request to star wars api
+  let starWarsCharacters = await axios.get('https://swapi.dev/api/people/?page=1');
+  //now lets get proof of life
+  // console.log('Galactic Heros: ',starWarsCharacters);
+  console.log('Galactic Heros: ',starWarsCharacters.data.results);
+  //this is the response object lets compare the data online and in console.
+  // we get back a promise. but why, because it is asyncroniesous so, we got to get js to chill for a second so we tell it its async
 
-  //4. Then I want to take that data and add it to state.
+
+
+  //5. Then I want to take that data and add it to state.
+  this.setState({
+    starWarsChars : starWarsCharacters.data.results
+  })
 
 };
 
   render() {
+    // 6. console log the added state
+    console.log('Star Wars STATE:',this.state.starWarsChars);
+    let startWarsList = this.state.starWarsChars.map((characterName, index) => {
+      return <li key={index}>{characterName.name}</li>;
+    })
+
+
+
     return (
       <>
         <h1>Data from an STAR WARS API</h1>
@@ -27,6 +49,8 @@ handleSubmit = (event) => {
         <form onSubmit={this.handleSubmit}>
           <button type="submit">Display Star Wars Data</button>
         </form>
+        {/* 7. add ul and the list variable */}
+        {startWarsList}
       </>
      
     )
