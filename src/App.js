64bcc72160c488809c255b066ae4bd6024c786bf
tektechnologies <info@tektechnologies.com>
2022-07-30 22:31:57 -0500

    add render jsx

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import React from 'react';
 class App extends React.Component {
 
 
-  
+
   render() {
     return (
+      <>
+        <h1>Data from an APi</h1>
+        <form>
+          <button>Display Star Wars Data</button>
+        </form>
+      </>
      
     )
   }
